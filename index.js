8dc6f08f7da707b506273ec436cbe842b0940913
Prasoon Rajpoot <betusingh890@gmail.com>
2023-06-04 20:52:21 +0530

    initial repositary

---
+++ index.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('canvas');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const c =   canvas.getContext('2d');
+
+
+// console.log(c);
+
+
+
+class Player{
+    constructor (x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        c.fillStyle = this.color;
+        c.fill()
+    }
+
+};
+
+const player = new Player(100, 100,30, 'blue');
+player.draw()
+console.log(player);
+
